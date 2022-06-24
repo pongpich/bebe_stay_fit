85c6c18817a80978b35f0e4c92b5967f8b866f36
Akkewach Yodsomboon <akkewach@planforfit.com>
2022-06-24 19:20:05 +0700

    หน้าโปรไฟล์ เม้นในส่วนที่ไม่เกี่ยวข้อง

--- src/views/profile/editProfile.js
+++ src/views/profile/editProfile.js
@@ -6,151 +6,151 @@ import user_circle from "../../assets/img/user_circle.svg";
 
 
 class EditProfile extends React.Component {
-    render() {
-        return (
-            <>
-                <div className="padding-top4 center">
-                    <p className="font-size6 bold color-protein"> แก้ไขข้อมูล</p>
-                </div>
-                <div className="col-12 col-sm-12 col-md-12 col-lg-12  center2 ">
-                    <div className="col-12 col-sm-12 col-md-6 col-lg-6 center2">
-                        <div className="box-protein margin-bottom1 padding-top2">
-                            <div className="padding-top img-ellipse17">
-                                <img src={ellipse17_2} alt="vector" />
-                            </div>
+  render() {
+    return (
+      <>
+        <div className="padding-top4 center">
+          <p className="font-size6 bold color-protein"> แก้ไขข้อมูล</p>
+        </div>
+        {/* <div className="col-12 col-sm-12 col-md-12 col-lg-12  center2 ">
+          <div className="col-12 col-sm-12 col-md-6 col-lg-6 center2">
+            <div className="box-protein margin-bottom1 padding-top2">
+              <div className="padding-top img-ellipse17">
+                <img src={ellipse17_2} alt="vector" />
+              </div>
 
-                            <div className="padding-top2">
-                                <div className="d-grid gap-2  mx-auto   col-10 col-sm-10  col-md-5 col-lg-5">
-                                    <button className="btn btn-outline-pink" type="button" >
-                                        เลือกรูป
+              <div className="padding-top2">
+                <div className="d-grid gap-2  mx-auto   col-10 col-sm-10  col-md-5 col-lg-5">
+                  <button className="btn btn-outline-pink" type="button" >
+                    เลือกรูป
                                     </button>
-                                </div>
-                            </div>
-                            <div className=" col-10 col-sm-10  col-md-5 col-lg-5 center2 padding-top2">
-                                <p className="font-size5  between">ขนาดไฟล์ <span className="bold">สูงสุด 1 MB</span></p>
-                                <p className="font-size5  between margin-top-1">ไฟล์ที่รองรับ <span className="bold">.JPEG, .PNG</span></p>
-                            </div>
-                        </div>
-                    </div>
                 </div>
-                <div className="col-12 col-sm-12 col-md-12 col-lg-12  center2 ">
-                    <div className="col-12 col-sm-12 col-md-6 col-lg-6 center2">
-                        <div className="box-protein margin-bottom1 padding-top2">
-                            <div className="box-proteinAddress padding-top">
-                                <p className="font-size6 bold color-protein "> ข้อมูลส่วนตัว</p>
-                                <div>
-                                    <label className="form-label bold font-size4">ชื่อแฝง</label>
-                                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="บพิตร์ เตชะวัฒนานันท์" />
-                                </div>
-                                <div className="padding-top2">
-                                    <label className="form-label bold font-size4">วัน/เดือน/ปี เกิด</label>
-                                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="5 มกราคม 2536" />
-                                </div>
-                                <div className="padding-top2">
-                                    <p className="font-weight">เพศ</p>
-                                    <div className="form-check form-check-inline">
-                                        <input className="form-check-input" type="radio" name="sex" id="inlineRadio1" value="ชาย" />
-                                        <label className="form-check-label"> ชาย </label>
-                                    </div>
-                                    <div className="form-check form-check-inline">
-                                        <input className="form-check-input" type="radio" name="sex" id="inlineRadio2" value="หญิง" />
-                                        <label className="form-check-label" > หญิง  </label>
-                                    </div>
-                                </div>
-                                <div className="padding-top2">
-                                    <label className="form-label bold font-size4">อีเมล</label>
-                                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="borpit.techa@gmail.com" disabled />
-                                </div>
-                                <div className="padding-top2">
-                                    <label className="form-label bold font-size4 between">เบอร์โทรศัพท์ <span className="font-size4 normal"> ใช้ในการยืนยันตัวตนเข้าบัญชี</span></label>
-                                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="******9595" disabled />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+              </div>
+              <div className=" col-10 col-sm-10  col-md-5 col-lg-5 center2 padding-top2">
+                <p className="font-size5  between">ขนาดไฟล์ <span className="bold">สูงสุด 1 MB</span></p>
+                <p className="font-size5  between margin-top-1">ไฟล์ที่รองรับ <span className="bold">.JPEG, .PNG</span></p>
+              </div>
+            </div>
+          </div>
+        </div> */}
+        {/* <div className="col-12 col-sm-12 col-md-12 col-lg-12  center2 ">
+          <div className="col-12 col-sm-12 col-md-6 col-lg-6 center2">
+            <div className="box-protein margin-bottom1 padding-top2">
+              <div className="box-proteinAddress padding-top">
+                <p className="font-size6 bold color-protein "> ข้อมูลส่วนตัว</p>
+                <div>
+                  <label className="form-label bold font-size4">ชื่อแฝง</label>
+                  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="บพิตร์ เตชะวัฒนานันท์" />
                 </div>
+                <div className="padding-top2">
+                  <label className="form-label bold font-size4">วัน/เดือน/ปี เกิด</label>
+                  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="5 มกราคม 2536" />
+                </div>
+                <div className="padding-top2">
+                  <p className="font-weight">เพศ</p>
+                  <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="sex" id="inlineRadio1" value="ชาย" />
+                    <label className="form-check-label"> ชาย </label>
+                  </div>
+                  <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="sex" id="inlineRadio2" value="หญิง" />
+                    <label className="form-check-label" > หญิง  </label>
+                  </div>
+                </div>
+                <div className="padding-top2">
+                  <label className="form-label bold font-size4">อีเมล</label>
+                  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="borpit.techa@gmail.com" disabled />
+                </div>
+                <div className="padding-top2">
+                  <label className="form-label bold font-size4 between">เบอร์โทรศัพท์ <span className="font-size4 normal"> ใช้ในการยืนยันตัวตนเข้าบัญชี</span></label>
+                  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="******9595" disabled />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> */}
 
-                <div className="col-12 col-sm-12 col-md-12 col-lg-12  center2 ">
-                    <div className="col-12 col-sm-12 col-md-6 col-lg-6 center2">
-                        <div className="box-protein margin-bottom1 padding-top2">
-                            <div className="box-proteinAddress padding-top">
-                                <p className="font-size6 bold color-protein "> ที่อยู่ในการจัดส่งสินค้า</p>
-                                <div className="row">
-                                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
-                                        <label className="form-label bold font-size4">ชื่อ</label>
-                                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="" />
-                                    </div>
-                                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
-                                        <label className="form-label bold font-size4">นามสกุล</label>
-                                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="" />
-                                    </div>
-                                </div>
-                                <div className="padding-top2">
-                                    <label className="form-label bold font-size4">เบอร์โทรศัพท์</label>
-                                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="" />
-                                </div>
-                                <div className="padding-top2">
-                                    <label className="form-label bold font-size4">ที่อยู่</label>
-                                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="กรอกบ้านเลขที่, หมู่, ซอย, อาคาร, ถนน และจัดสุงเกต(ถ้ามี)" />
-                                </div>
-                                <div className="padding-top2">
-                                    <label className="form-label bold font-size4">แขวง/ตำบล</label>
-                                    <select className="form-select" aria-label="Default select example">
-                                        <option >เลือก</option>
-                                        <option >Classic malt Flavor</option>
-                                    </select>
-                                </div>
-                                <div className="padding-top2">
-                                    <label className="form-label bold font-size4">เขต/อำเภอ</label>
-                                    <select className="form-select" aria-label="Default select example">
-                                        <option >เลือก</option>
-                                        <option >Classic malt Flavor</option>
-                                    </select>
-                                </div>
-                                <div className="padding-top2">
-                                    <label className="form-label bold font-size4">จังหวัด</label>
-                                    <select className="form-select" aria-label="Default select example">
-                                        <option >เลือก</option>
-                                        <option >Classic malt Flavor</option>
-                                    </select>
-                                </div>
-                                <div className="padding-top2">
-                                    <label className="form-label bold font-size4">รหัสไปรษณีย์</label>
-                                    <select className="form-select" aria-label="Default select example">
-                                        <option >เลือก</option>
-                                        <option>Classic malt Flavor</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <div className="col-12 col-sm-12 col-md-12 col-lg-12  center2 ">
+          <div className="col-12 col-sm-12 col-md-6 col-lg-6 center2">
+            <div className="box-protein margin-bottom1 padding-top2">
+              <div className="box-proteinAddress padding-top">
+                <p className="font-size6 bold color-protein "> ที่อยู่ในการจัดส่งสินค้า</p>
+                <div className="row">
+                  <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+                    <label className="form-label bold font-size4">ชื่อ</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="" />
+                  </div>
+                  <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+                    <label className="form-label bold font-size4">นามสกุล</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="" />
+                  </div>
                 </div>
-                <div className="col-12 col-sm-12 col-md-12 col-lg-12  center margin-top-2 ">
-                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
-                        <div className="padding-top2 padding-left3 distance margin-bottom1">
-                            <div className="form-check">
-                                <input className="form-check-input" type="checkbox" id="flexCheckDefault" data-bs-toggle="modal" data-bs-target="#exampleModal" />
-                                <label className="form-check-label">
-                                    ขอใบเสร็จรับเงิน/ใบกำกับภาษี
+                <div className="padding-top2">
+                  <label className="form-label bold font-size4">เบอร์โทรศัพท์</label>
+                  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="" />
+                </div>
+                <div className="padding-top2">
+                  <label className="form-label bold font-size4">ที่อยู่</label>
+                  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="กรอกบ้านเลขที่, หมู่, ซอย, อาคาร, ถนน และจัดสุงเกต(ถ้ามี)" />
+                </div>
+                <div className="padding-top2">
+                  <label className="form-label bold font-size4">แขวง/ตำบล</label>
+                  <select className="form-select" aria-label="Default select example">
+                    <option >เลือก</option>
+                    <option >Classic malt Flavor</option>
+                  </select>
+                </div>
+                <div className="padding-top2">
+                  <label className="form-label bold font-size4">เขต/อำเภอ</label>
+                  <select className="form-select" aria-label="Default select example">
+                    <option >เลือก</option>
+                    <option >Classic malt Flavor</option>
+                  </select>
+                </div>
+                <div className="padding-top2">
+                  <label className="form-label bold font-size4">จังหวัด</label>
+                  <select className="form-select" aria-label="Default select example">
+                    <option >เลือก</option>
+                    <option >Classic malt Flavor</option>
+                  </select>
+                </div>
+                <div className="padding-top2">
+                  <label className="form-label bold font-size4">รหัสไปรษณีย์</label>
+                  <select className="form-select" aria-label="Default select example">
+                    <option >เลือก</option>
+                    <option>Classic malt Flavor</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="col-12 col-sm-12 col-md-12 col-lg-12  center margin-top-2 ">
+          <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+            <div className="padding-top2 padding-left3 distance margin-bottom1">
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" id="flexCheckDefault" data-bs-toggle="modal" data-bs-target="#exampleModal" />
+                <label className="form-check-label">
+                  ขอใบเสร็จรับเงิน/ใบกำกับภาษี
                                 </label>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-12 col-sm-12 col-md-12 col-lg-12  center2  margin-top-3">
-                    <div className="row">
-                        <div className=" center2">
-                            <button type="button" className="btn bottom-outlinePink margin-top-4 col-lg-12">ยกเลิก</button>
-                        </div>
-                        <div className="">
-                            <button type="button" className="btn bottom-pink">บันทึก</button>
-                        </div>
-                    </div>
-                </div>
-            </>
+              </div>
+            </div>
+          </div>
+        </div> */}
+        <div className="col-12 col-sm-12 col-md-12 col-lg-12  center2  margin-top-3">
+          <div className="row">
+            <div className=" center2">
+              <button type="button" className="btn bottom-outlinePink margin-top-4 col-lg-12">ยกเลิก</button>
+            </div>
+            <div className="">
+              <button type="button" className="btn bottom-pink">บันทึก</button>
+            </div>
+          </div>
+        </div>
+      </>
 
-        );
-    }
+    );
+  }
 }
 
 export default EditProfile;
--- src/views/profile/profile.js
+++ src/views/profile/profile.js
@@ -4,68 +4,68 @@ import user_circle from "../../assets/img/user_circle.svg";
 import { Link } from 'react-router-dom';
 
 class Profile extends React.Component {
-    render() {
-        return (
-            <>
-                <div className="padding-top4 center">
-                    <p className="font-size6 bold color-protein"> โปรไฟล์</p>
-                </div>
-                <div className="col-12 col-sm-12 col-md-12 col-lg-12  center2 ">
-                    <div className="col-12 col-sm-12 col-md-6 col-lg-6 center2">
-                        <div className="box-protein margin-bottom1 padding-top2">
-                            <div className="padding-top img-ellipse17">
-                                <img src={ellipse17} alt="vector" />
-                            </div>
-                            <div className="padding-top2">
-                                <p className="section-size">บพิตร์ เตชะวัฒนานันท์</p>
-                                <p className="margin-top-1 bold">borpit.techa@gmail.com</p>
-                                <p className="margin-top-1 section-size">367/161 จรัญสนิทวงศ์33 แยก3</p>
-                                <p className="margin-top-1 section-size">แขวงบางขุนศรี เขตบางกอกน้อย</p>
-                                <p className="margin-top-1 section-size">กรุงเทพมหานคร 10700</p>
+  render() {
+    return (
+      <>
+        <div className="padding-top4 center">
+          <p className="font-size6 bold color-protein"> โปรไฟล์</p>
+        </div>
+        <div className="col-12 col-sm-12 col-md-12 col-lg-12  center2 ">
+          <div className="col-12 col-sm-12 col-md-6 col-lg-6 center2">
+            <div className="box-protein margin-bottom1 padding-top2">
+              <div className="padding-top img-ellipse17">
+                <img src={ellipse17} alt="vector" />
+              </div>
+              <div className="padding-top2">
+                <p className="section-size">บพิตร์ เตชะวัฒนานันท์</p>
+                <p className="margin-top-1 bold">borpit.techa@gmail.com</p>
+                <p className="margin-top-1 section-size">367/161 จรัญสนิทวงศ์33 แยก3</p>
+                <p className="margin-top-1 section-size">แขวงบางขุนศรี เขตบางกอกน้อย</p>
+                <p className="margin-top-1 section-size">กรุงเทพมหานคร 10700</p>
 
-                            </div>
-                            <p className="border-bottom margin-leftRight"></p>
-                            <div className="d-grid gap-2  mx-auto   col-10 col-sm-10  col-md-8 col-lg-8 margin-top-2">
-                                {/*  <button className="btn bottom-pink" type="button" >
+              </div>
+              <p className="border-bottom margin-leftRight"></p>
+              <div className="d-grid gap-2  mx-auto   col-10 col-sm-10  col-md-8 col-lg-8 margin-top-2">
+                {/*  <button className="btn bottom-pink" type="button" >
                                     แก้ไขข้อมูล
                                 </button> */}
-                                <Link to="/edit_profile" className="btn bottom-pink " type="button">แก้ไขข้อมูล</Link>
-                                <button className="btn bottom-outlinePink margin-top-3 " type="button" >
-                                    เปลี่ยนรหัสผ่าน
+                <Link to="/edit_profile" className="btn bottom-pink " type="button">แก้ไขข้อมูล</Link>
+                <button className="btn bottom-outlinePink margin-top-3 " type="button" >
+                  เปลี่ยนรหัสผ่าน
                                 </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-12 col-sm-12 col-md-12 col-lg-12  center2 ">
-                    <div className="col-12 col-sm-12 col-md-6 col-lg-6 center2">
-                        <div className="box-protein margin-bottom1 padding-top2">
-                            <div className="padding-top">
-                                <h4 className="color1 bold">BEBEStayFit</h4>
-                            </div>
-                            <div className="padding-top2  margin-leftRight">
-                                <p className="font-size6 text-left2 bold">แพ็คเกจของคุณ </p>
-                                <p className="font-size0 margin-top-1 bold between">ตามระยะเวลาของโปรแกรม <span className="color1 bold font-size0">3,750 บาท / 2 เดือน</span></p>
-                                <p className=" font-size4 margin-top-1 right color3">วันตัดรอบบิล 30 มิ.ย. 2565</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="col-12 col-sm-12 col-md-12 col-lg-12  center2 ">
+          <div className="col-12 col-sm-12 col-md-6 col-lg-6 center2">
+            <div className="box-protein margin-bottom1 padding-top2">
+              <div className="padding-top">
+                <h4 className="color1 bold">BEBEStayFit</h4>
+              </div>
+              <div className="padding-top2  margin-leftRight">
+                <p className="font-size6 text-left2 bold">แพ็คเกจของคุณ </p>
+                <p className="font-size0 margin-top-1 bold between">ตามระยะเวลาของโปรแกรม <span className="color1 bold font-size0">3,750 บาท / 2 เดือน</span></p>
+                <p className=" font-size4 margin-top-1 right color3">วันตัดรอบบิล 30 มิ.ย. 2565</p>
 
-                            </div>
-                            <p className="border-bottom margin-leftRight padding-top2"></p>
-                            <div className="d-grid gap-2  mx-auto   col-10 col-sm-10  col-md-8 col-lg-8 margin-top-2 ">
-                                <button className="btn bottom-pink " type="button" onClick={() => this.props.history.push('/billing_history')}>
-                                    ประวัติการเรียกเก็บ
+              </div>
+              <p className="border-bottom margin-leftRight padding-top2"></p>
+              <div className="d-grid gap-2  mx-auto   col-10 col-sm-10  col-md-8 col-lg-8 margin-top-2 ">
+                <button className="btn bottom-pink " type="button" onClick={() => this.props.history.push('/billing_history')}>
+                  ประวัติการเรียกเก็บ
                                 </button>
-                                <button className="btn bottom-outlineGrey margin-top-3" type="button" >
-                                    ยกเลิกแพ็คเกจ
+                <button className="btn bottom-outlineGrey margin-top-3" type="button" >
+                  ยกเลิกแพ็คเกจ
                                 </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              </div>
+            </div>
+          </div>
+        </div> */}
 
-            </>
+      </>
 
-        );
-    }
+    );
+  }
 }
 
 export default Profile;
