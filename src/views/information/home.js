59304de82181d8b8651262f524bf18323351afcd
THANETNove <Novel_Manga@outlook.co.th>
2022-06-28 17:28:06 +0700

    หน้า home ขนาด จอคอม

--- src/views/information/home.js
+++ src/views/information/home.js
@@ -5,6 +5,21 @@ import { getUserProgram } from "../../redux/exerciseProgram";
 import { loginUser, resetStatusSetPassword } from "../../redux/auth";
 import backgroundImag from '../../assets/img/mainbg.jpeg';
 import loginprofile from '../../assets/img/loginprofile.jpg';
+import head from '../../assets/img/home/head.webp';
+import part3 from '../../assets/img/home/part3.webp';
+import part4 from '../../assets/img/home/part4.webp';
+import part5 from '../../assets/img/home/part5.webp';
+import part6 from '../../assets/img/home/part6.webp';
+import part7 from '../../assets/img/home/part7.webp';
+import part8 from '../../assets/img/home/part8.webp';
+import part9 from '../../assets/img/home/part9.webp';
+import part10 from '../../assets/img/home/part10.webp';
+import part11 from '../../assets/img/home/part11.webp';
+import review from '../../assets/img/home/review.webp';
+import part13 from '../../assets/img/home/part13.webp';
+import price from '../../assets/img/home/price.webp';
+import countdown from '../../assets/img/home/countdown.webp';
+import part16 from '../../assets/img/home/part16.webp';
 
 class Home extends React.Component {
   constructor(props) {
@@ -89,95 +104,32 @@ class Home extends React.Component {
   homeLogin() {
     return (
       <>
-        <div>
-          <div className="col-12 col-sm-12 col-md-12 col-lg-12 box-Null">
-            <div className="row ">
-              <div className="col-12 col-sm-12 col-md-6 col-lg-6 center">
-                <div className="box-NullIn">
-                </div>
-              </div>
-              <div className="col-12 col-sm-12 col-md-6 col-lg-6 center">
-                <div className="box-NullIn">
-
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 col-sm-12 col-md-12 col-lg-12 box-Null1">
-            <div className="row ">
-              <div className="col-12 col-sm-12 col-md-6 col-lg-6 center">
-                <div className="box-NullIn">
-                  <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal" >Login</button> &nbsp; &nbsp;
-                  <Link to="/programPackage" className="btn btn-secondary" type="button">คลิก</Link>
-                  <Link to="/profile" className="btn btn-secondary" type="button">profile</Link>
-                </div>
-              </div>
-              <div className="col-12 col-sm-12 col-md-6 col-lg-6 center">
-                <div className="box-NullIn">
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 col-sm-12 col-md-12 col-lg-12 box-Null">
-            <div className="row ">
-              <div className="col-12 col-sm-12 col-md-6 col-lg-6 center">
-                <div className="box-NullIn">
-                </div>
-              </div>
-              <div className="col-12 col-sm-12 col-md-6 col-lg-6 center">
-                <div className="box-NullIn">
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-              <div className="modal-content padding-leftRight">
-                <div className="modal-headerIn margin-headText">
-                  <p className="bold font-size5  color-protein" id="exampleModalLabel"></p>
-                  <button type="button" className="btn-close" id="remove-model" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div className="modal-bodyIn">
-                  <div className="center margin-bottom margin-top-1">
-                    <p className="bold font-size8  color-protein" id="exampleModalLabel">เข้าสู่ระบบ</p>
-                  </div>
-                  <div className=" col-12 col-sm-12  col-md-12 col-lg-12 padding-top1">
-                    <div className="mb-3">
-                      <label className="form-label">อีเมล</label>
-                      <input
-                        type="email"
-                        className="form-control"
-                        id="email"
-                        placeholder="กรุณากรอก Email (Example@mail.com)"
-                        value={this.state.email}
-                        onChange={(event) => this.handleChange(event)}
-                      />
-                    </div>
-                    <div className="mb-3">
-                      <label className="form-label">รหัสผ่าน</label>
-                      <input
-                        type="password"
-                        className="form-control"
-                        id="password"
-                        value={this.state.password}
-                        onChange={(event) => this.handleChange(event)}
-                      />
-                    </div>
-                    {this.state.validation !== "true" ?
-                      <h6 style={{ color: "red" }}>กรุณากรอกข้อมูลให้ครบถ้วน</h6>
-                      : null}
-                    <div className="d-grid gap-2  mx-auto   col-12 col-sm-12  col-md-12 col-lg-12 distance">
-                      <button className="btn bottom-pinkLogin   font-size6" type="button" onClick={() => this.onUserLogin()}>
-                        เข้าสู่ระบบ
-                      </button>
-                    </div>
-                    <p className="between margin-top-2 font-size4"><span className="pointer reset" onClick={() => this.resetPassword()}>ลืมรหัสผ่าน</span> <span>ยังไม่เป็นสมาชิก? <a href="#">ลงทะเบียน</a></span></p>
-                  </div>
-                </div>
-              </div>
+        <img src={head} alt="vector" className="home-image" />
+        <div className="box-home">
+          <div className="top-Home">
+            <p className="textHome bold">สนใจสมัคร</p>
+            <p className="textHome2 bold" >ปรึกษาผู้เชี่ยวชาญ</p>
+            <div className="d-grid gap-2  mx-auto   col-12 col-sm-12  col-md-12 col-lg-12 center ">
+              <button className="btn bottom-pinkLogin2 bold" type="button" onClick={() => this.onUserLogin()}>
+                เข้าสู่ระบบ
+              </button>
             </div>
           </div>
         </div>
+        <img src={part3} alt="vector" className="home-image" />
+        <img src={part4} alt="vector" className="home-image" />
+        <img src={part5} alt="vector" className="home-image" />
+        <img src={part6} alt="vector" className="home-image" />
+        <img src={part7} alt="vector" className="home-image" />
+        <img src={part8} alt="vector" className="home-image" />
+        <img src={part9} alt="vector" className="home-image" />
+        <img src={part10} alt="vector" className="home-image" />
+        <img src={part11} alt="vector" className="home-image" />
+        <img src={review} alt="vector" className="home-image" />
+        <img src={part13} alt="vector" className="home-image" />
+        <img src={price} alt="vector" className="home-image" />
+        <img src={countdown} alt="vector" className="home-image" />
+        <img src={part16} alt="vector" className="home-image" />
       </>
     )
   }
@@ -248,8 +200,8 @@ class Home extends React.Component {
     const { validation } = this.state;
     return (
       <>
-      {/*      {this.homeLogin()} */}
-        {this.login()}
+        {this.homeLogin()}
+        {/*  {this.login()} */}
       </>
     );
   }
