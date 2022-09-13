6b5d5b04a3223861c1692c221564ecfe873c351a
THANETNove <Novel_Manga@outlook.co.th>
2022-09-13 11:22:19 +0700

    upcode

--- src/views/information/challenge.js
+++ src/views/information/challenge.js
@@ -416,10 +416,14 @@ class Challenge extends Component {
             challengePeriod ?
               <>
               <nav>
-              <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true"><IntlMessages id="challenge.teamChallenge" /></button>
-                <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false"><IntlMessages id="challenge.singleChallenge" /></button>
-              </div>
+             {/*  <div className="video-wh4"> */}
+                  <div class="nav nav-tabs video-wh4" id="nav-tab" role="tablist">
+                  
+                    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true"><IntlMessages id="challenge.teamChallenge" /></button>
+                    <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false"><IntlMessages id="challenge.singleChallenge" /></button>
+                  </div>
+                {/* </div> */}
+              
             </nav>
             <div class="tab-content" id="nav-tabContent">
               <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
