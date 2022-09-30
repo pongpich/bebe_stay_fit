b8e2b43f78fa120d9881a5c1b32e456f295f3cac
Akkewach Yodsomboon <akkewach@planforfit.com>
2022-09-30 17:57:31 +0700

    แก้เงื่อนไขการโชว์ปุ่มยกเลิกแพ็คเกจ และ วันตัดรอบบิล

--- src/views/profile/profile.js
+++ src/views/profile/profile.js
@@ -10,7 +10,6 @@ class Profile extends React.Component {
 
   componentDidMount() {
     const { user } = this.props;
-    console.log("user.user_id", user.user_id);
     this.props.getSubscriptionProducts(user.user_id);
     this.props.getRegister_log(user.user_id);
   }
@@ -19,7 +18,6 @@ class Profile extends React.Component {
     const address = JSON.parse(this.props.delivery_address);
     const program = this.props.register_log;
 
-    console.log("program", program);
     /*   const program_id =  this.props.register_log.program_id;
       const round =  this.props.register_log.round;
       const expire_date =  new Date(this.props.register_log.expire_date);
@@ -52,7 +50,7 @@ class Profile extends React.Component {
                 {/*  <button className="btn bottom-pink" type="button" >
                                     แก้ไขข้อมูล
                                 </button> */}
-                <button className="btn bottom-outlinePink3" type="button"  onClick={() => this.props.history.push("/edit_profile")}><IntlMessages id="profile.editInformation" /></button>
+                <button className="btn bottom-outlinePink3" type="button" onClick={() => this.props.history.push("/edit_profile")}><IntlMessages id="profile.editInformation" /></button>
                 <button className="btn bottom-outlinePink margin-top-3 " type="button" onClick={() => this.props.history.push("/reset_password")} >
                   <IntlMessages id="profile.changePassword" />
                 </button>
@@ -65,11 +63,12 @@ class Profile extends React.Component {
 
 
           program && program.map((item, i) => {
-            console.log("item", item);
             const il = program.length - 1;
             if (il === i) {
-              const expire_date = new Date(item.expire_date);
-              const date = expire_date.toLocaleString("th-TH", {
+              console.log("item.created_at :", item.created_at);
+              const created_at = new Date(item.created_at);
+              console.log("created_at :", created_at);
+              const date = created_at.toLocaleString("th-TH", {
                 year: 'numeric',
                 month: 'long',
                 day: 'numeric',
@@ -77,7 +76,7 @@ class Profile extends React.Component {
               const name = (
                 <>
                   {
-                    (item.round > 1)  ? /* && (item.payment_type === 'credit_card') ? */ //เช็คว่า round ล่าสุดมากกว่า 1 และ payment_type เป็นบัตรเครดิต
+                    (item.round > 1) ? /* && (item.payment_type === 'credit_card') ? */ //เช็คว่า round ล่าสุดมากกว่า 1 และ payment_type เป็นบัตรเครดิต
                       <>
                         <div className="col-12 col-sm-12 col-md-12 col-lg-12  center2 ">
                           <div className="col-12 col-sm-8 col-md-6 col-lg-4 center2">
@@ -87,13 +86,13 @@ class Profile extends React.Component {
                               </div>
                               <div className="padding-top2 left-text">
                                 <p className="font-size6 text-left2 bold between"><IntlMessages id="payment.yourPackage" />  <span className="pay-user"><IntlMessages id="payment.pay" /></span></p>
+                                <p className="font-size0 margin-top-1 bold between"><IntlMessages id="payment.applyProgram" /> <span className="color1 bold font-size0">1,800 <IntlMessages id="programPackage.baht" /> / <IntlMessages id="programPackage.months" /></span></p>
                                 {
-                                  item.program_id === "subscription_stay_fit_01" ?
-                                    <p className="font-size0 margin-top-1 bold between"><IntlMessages id="payment.applyProgram" /> <span className="color1 bold font-size0">1,800 <IntlMessages id="programPackage.baht" /> / <IntlMessages id="programPackage.months" /></span></p>
+                                  (item.payment_type === 'credit_card') ?
+                                    <p className=" font-size4 margin-top-1 right color3"><IntlMessages id="profile.cut-offdate" />  {date}</p>
                                     :
-                                    <p className="font-size0 margin-top-1 bold between"><IntlMessages id="payment.applyProgram" /> <span className="color1 bold font-size0">3,990 <IntlMessages id="programPackage.baht" /></span></p>
+                                    null
                                 }
-                                <p className=" font-size4 margin-top-1 right color3"><IntlMessages id="profile.cut-offdate" />  {date}</p>
                               </div>
                               <p className="border-bottom margin-leftRight padding-top2"></p>
                               <div className="d-grid gap-2  mx-auto   col-10 col-sm-10  col-md-8 col-lg-8 margin-top-2 ">
@@ -104,13 +103,14 @@ class Profile extends React.Component {
                                   ยกเลิกแพ็กเกจ
                                 </button> */}
                                 {
-                                  (item.payment_type !== 'credit_card')  ?
-                                  <button className="btn bottom-outlineGrey margin-top-3" type="button" onClick={() => this.props.history.push('/cancel_package_new')}>
-                                  ยกเลิกแพ็กเกจ
-                                </button>
-                                 :null
+                                  (item.payment_type === 'credit_card') ?
+                                    <button className="btn bottom-outlineGrey margin-top-3" type="button" onClick={() => this.props.history.push('/cancel_package_new')}>
+                                      ยกเลิกแพ็กเกจ
+                                    </button>
+                                    :
+                                    null
                                 }
-                                
+
                               </div>
                             </div>
                           </div>
